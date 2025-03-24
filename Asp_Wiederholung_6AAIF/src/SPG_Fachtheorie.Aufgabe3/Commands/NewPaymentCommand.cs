041725fb37b2d635fdad0e5e84ab5c3da60d858b
Alberd Batajev <BAT220488@spengergasse.at>
2025-03-24 14:29:29 +0100

    Aufgabe 3

---
+++ Asp_Wiederholung_6AAIF/src/SPG_Fachtheorie.Aufgabe3/Commands/NewPaymentCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+public class NewPaymentCommand
+{
+    [Required]
+    public int CashDeskNumber { get; set; }
+
+    [Required]
+    [DataType(DataType.DateTime)]
+    public DateTime PaymentDateTime { get; set; }
+
+    [Required]
+    public string PaymentType { get; set; }
+
+    [Required]
+    public int EmployeeRegistrationNumber { get; set; }
+}
